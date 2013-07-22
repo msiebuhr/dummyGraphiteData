361ffe297ce72024cc27c05d22de95378c949bc9
Morten Siebuhr <msi@one.com>
2013-07-22 11:08:20 +0200

    Main: Generate list of metric and iterate over that.

--- main.go
+++ main.go
@@ -17,10 +17,9 @@ name, random value and the previosu metrics' timestamp bumped by one second)
 TODO
 
  * Specify host
- * Specify number of different metrics to send
- * User should supply number of different metrics to send
- * Loop over generated metric names, use Perlin-like noise as values.
+ * Use Perlin-like noise as values.
  * Randomly skip values
+ * Generate same set of metrics each time.
 
 */
 package main
@@ -38,11 +37,13 @@ import (
 var minNameParts int
 var maxNameParts int
 var maxMetrics int64
+var differentMetrics int
 
 func init() {
 	flag.IntVar(&minNameParts, "min", 2, "Minimum number of metric name elements")
 	flag.IntVar(&maxNameParts, "max", 3, "Maximum number of metric name elements")
 	flag.Int64Var(&maxMetrics, "n", 1<<60, "Number of metrics to send")
+	flag.IntVar(&differentMetrics, "m", 100, "Number of different metrics to generate")
 }
 
 var nameparts = []string{
@@ -50,6 +51,43 @@ var nameparts = []string{
 	"bar",
 	"baz",
 	"qux",
+	"frob",
+}
+
+// Simple power function on integers
+func intPower(x, y int) int {
+	res := 1
+	for i := 1; i <= y; i++ {
+		res = res * x
+	}
+	return res
+}
+
+// Generate a list of metric names of specified length
+func generateMetricNames() []string {
+	// How many can we generate witht the given constraints
+	num := 0
+	for i := minNameParts; i <= maxNameParts; i++ {
+		num += intPower(len(nameparts), i)
+	}
+	if num < differentMetrics {
+		fmt.Println("Can only generate", num, "combinations, asked to do", differentMetrics)
+		os.Exit(1)
+	}
+
+	// Create a map and fill it with random data
+	m := make(map[string]bool, differentMetrics)
+	for len(m) < differentMetrics {
+		m[generateMetricName()] = true
+	}
+
+	// Extract keys and return those
+	l := []string{}
+	for key := range m {
+		l = append(l, key)
+	}
+
+	return l
 }
 
 func generateMetricName() string {
@@ -64,15 +102,6 @@ func generateMetricName() string {
 	return strings.Join(parts, ".")
 }
 
-func generateMetric(timeOffset int64) string {
-	return fmt.Sprintf(
-		"%v %.5v %d",
-		generateMetricName(),
-		rand.NormFloat64()*5+5, // Mostly 0 to 10
-		time.Now().Unix()+timeOffset,
-	)
-}
-
 func main() {
 	// Read options
 	flag.Parse()
@@ -81,6 +110,7 @@ func main() {
 	}
 	if minNameParts > maxNameParts {
 		fmt.Println("Error: -min (%v) should be smaller than -max (%v)", minNameParts, maxNameParts)
+		os.Exit(1)
 	}
 
 	// Dial up a server
@@ -91,15 +121,28 @@ func main() {
 	}
 
 	index := int64(0)
+	timeOffset := int64(0)
+	metricNames := generateMetricNames()
 	max := maxMetrics
 
 	// Send data
 	for index < max {
-		metric := generateMetric(index)
-		fmt.Println(metric)
-		fmt.Fprintf(conn, metric)
-		fmt.Fprintf(conn, "\n")
-		index++
+		for _, metricName := range metricNames {
+			// Stop if we've generated enough
+			if index >= max {
+				break
+			}
+			metric := fmt.Sprintf(
+				"%v %.5v %d\n",
+				metricName,
+				rand.NormFloat64()*5+5, // Mostly 0 to 10
+				time.Now().Unix()+timeOffset,
+			)
+			fmt.Print(metric)
+			fmt.Fprint(conn, metric)
+			index++
+		}
+		timeOffset++
 	}
 	os.Exit(0)
 }
